d7689ed8bc77831dc441b05dff74fd1e7bd00a55
thomson02 <thomson02@hotmail.com>
2016-05-08 15:47:35 +0100

    added unit test proj

---
+++ StockMarket.UnitTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" company="Thomson02">
+//   
+// </copyright>
+// <summary>
+//   Defines the UnitTest1 type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace StockMarket.UnitTests
+{
+    using System;
+    using System.Reflection;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Ninject;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            // Configure Ninject
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
