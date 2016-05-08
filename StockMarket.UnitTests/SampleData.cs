3c1ea1b07142d4c0cb418c06b2bfa018265b6c4d
thomson02 <thomson02@hotmail.com>
2016-05-08 17:36:37 +0100

    add sample data to tests

---
+++ StockMarket.UnitTests/SampleData.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SampleData.cs" company="Thomson02">
+//    Copyright © Thomson02. All rights reserved.
+// </copyright>
+// <summary>
+//   The sample data.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace StockMarket.UnitTests
+{
+    using System.Collections.Generic;
+    using Thomson02.GBCE.CoreTypes.Stock;
+
+    /// <summary>
+    /// The sample data.
+    /// </summary>
+    public static class SampleData
+    {
+        /// <summary>
+        /// The stocks.
+        /// </summary>
+        /// <returns>The <see cref="Dictionary"/>.</returns>
+        public static Dictionary<string, Stock> Stocks()
+        {
+            return new Dictionary<string, Stock>
+                       {
+                           { "TEA", new CommonStock("TEA", 0, 100) },
+                           { "POP", new CommonStock("POP", 8, 100) },
+                           { "ALE", new CommonStock("ALE", 23, 60) },
+                           { "GIN", new PreferredStock("GIN", 8, 100, 2) },
+                           { "JOE", new CommonStock("JOE", 13, 250) },
+                       };
+        } 
+    }
+}
---
+++ StockMarket.UnitTests/StockMarketUnitTests.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StockMarketUnitTests.cs" company="Thomson02">
+//    Copyright © Thomson02. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the StockMarketUnitTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace StockMarket.UnitTests
+{
+    using System.Reflection;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Ninject;
+    using Thomson02.GBCE;
+
+    /// <summary>
+    /// The stock market unit tests.
+    /// </summary>
+    [TestClass]
+    public class StockMarketUnitTests
+    {
+        /// <summary>
+        /// The stock market.
+        /// </summary>
+        private StockMarket stockMarket;
+
+        /// <summary>
+        /// The setup of the unit tests.
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            // Configure Ninject
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            // Configure the Stock Market to use GBCE Data
+            kernel.Rebind<StockMarket>()
+                .ToSelf()
+                .InSingletonScope()
+                .WithConstructorArgument("stockCatalogue", SampleData.Stocks());
+
+            this.stockMarket = kernel.Get<StockMarket>();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
