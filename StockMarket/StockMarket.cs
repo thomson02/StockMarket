891ed96f44e142105ad21b56ed92e69efa7b07e0
thomson02 <thomson02@hotmail.com>
2016-05-08 14:48:54 +0100

    skeleton stockmarket.

---
+++ StockMarket/StockMarket.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StockMarket.cs" company="Thomson02">
+//    Copyright © Thomson02. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the StockMarket type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Thomson02.GBCE
+{
+    /// <summary>
+    /// The stock market (singleton).
+    /// </summary>
+    public sealed class StockMarket
+    {
+        private StockMarket()
+        {
+            
+        }
+
+        public static StockMarket Instance { get; } = new StockMarket();
+    }
+}
